4eff73440ef59032d40754d92bb2f61617b61807
b2blosangeles <b2blosangeles@gmail.com>
2020-06-08 09:30:44 -0700

    update

--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
-echo 'build.sh'
+echo 'Start docker app ..'
+FD="$PWD"
+cd dockerFiles/
+docker build -f  angular11 -t docker_ui_image .
+docker container stop docker_ui_container
+docker container rm docker_ui_container
+docker run -d -v "app":/var/_dockerApp  --name docker_ui_container  docker_ui_image
+# docker run -d --name docker_ui_container  docker_ui_image
+echo "$FD/app"
---
+++ dockerFiles/_entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir -p app
+cd app
+# npm start &
+# npm run server &
+while true; do
+    echo "sleep 5"
+done
