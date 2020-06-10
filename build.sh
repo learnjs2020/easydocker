df294be227566e64e89589d3d5719ad965e33c4b
b2blosangeles <b2blosangeles@gmail.com>
2020-06-09 23:32:20 -0700

    update

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ cd dockerFiles
 docker build -f  angular11 -t docker_ui_image .
 docker container stop docker_ui_container
 docker container rm docker_ui_container
-docker run -d -v "$FD/app":/var/_dockerApp  --name docker_ui_container  docker_ui_image
+docker run -d -p 3000:3000 -p 4200:4200 -v "$FD/app":/var/_dockerApp  --name docker_ui_container  docker_ui_image
