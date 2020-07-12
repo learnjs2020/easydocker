ee2c73b9269b4b9c01bae8e2ba609813bf2d1923
b2blosangeles <b2blosangeles@gmail.com>
2020-07-12 12:15:51 -0700

    update

--- runDocker.sh
+++ runDocker.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-SITE=$1
-SCRIPTDIR=$(cd `dirname $0` && pwd)
-SITE_PATH="$(dirname "$SCRIPTDIR")/apps/sites/$SITE"
-
-echo $SCRIPTDIR
-echo $SITE_PATH
-
-DOCKER_CFG="$SITE_PATH/dockerSetting.json"
-
-echo $DOCKER_CFG
-
-# if [ -f "$f" ]; then
-
 function jsonValue() {
     KEY=$1
     #num=$2
@@ -20,6 +7,21 @@ function jsonValue() {
 }
 
 
-echo $(cat $DOCKER_CFG) | jsonValue port
-echo $1
-exit 1
+SITE=$1
+SCRIPTDIR=$(cd `dirname $0` && pwd)
+SITE_PATH="$SCRIPTDIR/apps/sites/$SITE"
+
+echo $SCRIPTDIR
+echo $SITE_PATH
+ 
+cd $SITE_PATH &> /dev/null && git pull &> /dev/null && cd $SCRIPTDIR &> /dev/null
+
+DOCKER_CFG="$SITE_PATH/dockerSetting.json"
+
+echo $DOCKER_CFG
+
+if [ -f "$DOCKER_CFG" ]; then
+    echo $(cat $DOCKER_CFG) | jsonValue port
+    echo $(cat $DOCKER_CFG) | jsonValue ip
+    echo $1
+fi
